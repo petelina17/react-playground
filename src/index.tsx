790ff7dd4e019bd3d547ed2a9814da88f8da30fc
petelina17 <petelina17@gmail.com>
2020-02-16 15:50:28 +0100

    init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import './index.css'
+
+import App from './components/app';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("wrapper")
+);
